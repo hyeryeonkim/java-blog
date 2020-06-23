045fd21c7278f3c6a765557f5e74005f2f2197ab
hyeryeonkim <kim5638yw@naver.com>
2020-06-23 12:38:07 +0900

    상단바 작업

---
+++ part/head.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Harry's Blog</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <link rel="stylesheet" href="/resource/common.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/resource/common.js"></script>
+</head>
+
+<body>
+<div class="top-bar visible-on-md-up">
+    <div class="con height-100p flex flex-jc-sb">
+      <a href="#" class="logo flex flex-ai-c">
+        <i class="fab fa-angellist"></i>
+      </a>
+
+      <nav class="menu-box-1 con">
+        <ul class="flex height-100p">
+          <li>
+            <a href="../index.php" class="height-100p flex flex-ai-c">Home</a>
+              
+            </li>
+          <li><a href="../articles.php" class="height-100p flex flex-ai-c">Articles</a></li>
+          <li><a href="../aboutMe.php" class="height-100p flex flex-ai-c">About Me</a></li>
+          <li>
+            <a href="#" class="height-100p flex flex-ai-c">CONTACT</a>
+            <ul>
+              <li><a class="block" href="https://github.com/hyeryeonkim" target="_blank ">GITHUB</a></li>
+              <li><a class="block" href="https://harrydony.tistory.com/" target="_blank">TISTORY</a></li>
+
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+
+  <div class="mobile-top-bar visible-on-sm-down flex flex-ai-c">
+    <a href="#" class="btn-toggle-mobile-side-bar">
+      <div></div>
+      <div></div>
+      <div></div>
+    </a>
+    <a href="#" class="logo flex flex-ai-c absolute-center absolute-middle">
+      <i class="fab fa-angellist"></i>
+    </a>
+  </div>
+
+  <div class="mobile-side-bar flex flex-ai-c visible-on-sm-down">
+
+    <nav class="menu-box-1 flex-grow-1">
+      <ul>
+        <li><a href="#" class="block">Home</a></li>
+        <li><a href="#" class="block">Articles</a></li>
+        <li><a href="#" class="block">About Me</a></li>
+        <li>
+          <a href="#" class="block">CONTACT</a>
+          <ul>
+            <li><a class="block" href="https://github.com/hyeryeonkim" target="_blank">GITHUB</a></li>
+            <li><a class="block" href="https://harrydony.tistory.com/" target="_blank">TISTORY</a></li>
+
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+include "../part/head.php";
+?>
+<h1 class="con">Main</h1>
+
+<?php
+include "../part/foot.php";
+?>
